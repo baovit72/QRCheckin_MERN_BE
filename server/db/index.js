0cddf1cdd63a8b56908583dab7fe809501e8e90d
Hu Yuan Bao <nguyenbaont2212@gmail.com>
2020-03-20 14:14:54 +0700

    update connection string

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose
-    .connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/diemdanh', { useNewUrlParser: true })
+    .connect(process.env.MONGOLAB_URI || 'mongodb://127.0.0.1:27017/diemdanh', { useNewUrlParser: true })
     .catch(e => {
         console.error('Connection error', e.message)
     })
