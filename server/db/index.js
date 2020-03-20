2b0feb71552e41cfd2c5100b1338e59508ee6777
Hu Yuan Bao <nguyenbaont2212@gmail.com>
2020-03-20 14:04:46 +0700

    update remote db

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose
-    .connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/diemdanh', { useNewUrlParser: true })
+    .connect('mongodb+srv://baovit72:hnb2212@cluster0-tp0k7.mongodb.net/test?retryWrites=true&w=majority' ||process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/diemdanh', { useNewUrlParser: true })
     .catch(e => {
         console.error('Connection error', e.message)
     })
